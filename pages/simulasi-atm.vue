17f66fa72e613065042028f8ff5577158e02f66f
fatkulnurk <fatkulnurk@gmail.com>
2020-02-11 01:22:27 +0700

    fixing bug and layout

--- pages/simulasi-atm.vue
+++ pages/simulasi-atm.vue
@@ -15,7 +15,13 @@
           <div class="box">
             <h3 class="subtitle is-5">Simulasi ATM</h3>
             <div class="columns">
-              <div class="column is-12">
+              <div class="column is-6">
+                <div class="field">
+                  <label>Kode Perusahaan</label>
+                  <input type="number" class="input">
+                </div>
+              </div>
+              <div class="column is-6">
                 <div class="field">
                   <label>Nomor Pembayaran</label>
                   <input type="number" class="input" v-model="kodePembayaran">
@@ -26,33 +32,23 @@
               <button class="button is-primary is-fullwidth" v-on:click="showPaymentDetail">Cek Transaksi</button>
             </div>
           </div>
-          <div class="notification is-danger" v-if="showNotificationSection">
-            Nomor pembayaran tidak ditemukan atau sudah tidak berlaku.
+          <div v-if="showNotificationSection == true">
+            <h1>ijkashajkshjaks</h1>
+            <div class="notification is-danger">
+              Nomor pembayaran tidak ditemukan atau sudah tidak berlaku.
+            </div>
+          </div>
+
+          <div class="notification is-success" v-if="showNotificationSuccessPayment">
+            <p>Pembayaran sukses.</p>
           </div>
 
           <div class="box content" v-if="showPaymentSection">
-            <h3 class="subtitle is-4">Pembayaran</h3>
-            <p>Silakan lakukan pembayaran ke Rekening Bank BRI di bawah ini:</p>
-            <p><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/BANK_BRI_logo.svg/1280px-BANK_BRI_logo.svg.png" style="max-width: 128px"></p>
-            <table class="table is-fullwidth">
-              <tr>
-                <td>Kode Perusahaan</td>
-                <td>:</td>
-                <td></td>
-              </tr>
-              <tr>
-                <td>Nomor Pembayaran</td>
-                <td>:</td>
-                <td></td>
-              </tr>
-              <tr>
-                <td>Jumlah / Nominal</td>
-                <td>:</td>
-                <td></td>
-              </tr>
-            </table>
+            <p>Jumlah yang harus anda bayar adalah: </p>
+            <h3 class="subtitle is-4">Rp {{ bankPaymentInformation.amount}}</h3>
+            <hr>
             <div class="field">
-              <button class="button is-primary is-fullwidth">Bayar</button>
+              <button class="button is-primary is-fullwidth" v-on:click="verificationPayment">Bayar</button>
             </div>
           </div>
         </div>
@@ -72,13 +68,27 @@
         showPaymentSection: false,
         showNotificationSection: false,
         kodePembayaran: 0,
-        amount: 0
+        bankPaymentInformation: {amount: ''},
+        showNotificationSuccessPayment: false
       }
     },
     methods: {
       showPaymentDetail() {
-        this.showPaymentSection = true
-        this.showNotificationSection = true
+        this.$axios.get('/core/users/check_order/?customer_code=' + this.kodePembayaran).then( response => {
+          this.bankPaymentInformation = response.data
+          console.log(this.bankPaymentInformation)
+          this.showPaymentSection = true
+        }).catch(error => function () {
+          this.showNotificationSection = true
+          this.showPaymentSection = false;
+          return ''
+        })
+      },
+      verificationPayment() {
+        this.$axios.post('/core/users/finish_order/', {customer_code: this.kodePembayaran}).then(response => {
+          this.showNotificationSuccessPayment = true;
+          console.log(response.data)
+        })
       }
     }
   }
